b82ed5e0d06ee81ab6f88abcd6199be54d89a1e5
DeFreitas-Samuel <52897285+DeFreitas-Samuel@users.noreply.github.com>
2023-03-16 01:39:52 -0400

    reformated the code and now it uses reactive forms in the signup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import { SignupComponent } from './signup/signup.component';
-import { FormInputComponent } from './shared/form-input/form-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent,
-    FormInputComponent
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatButtonModule
   ],
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from "../Models/User";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -8,13 +9,26 @@ import {User} from "../Models/User";
 })
 export class SignupComponent implements OnInit {
   user: User = new User();
-  constructor() { }
+  signUpForm: FormGroup;
+
+  constructor() {
+    this.signUpForm = new FormGroup({
+      'firstname': new FormControl(null, Validators.required),
+      'lastname': new FormControl(null, Validators.required),
+      'address1': new FormControl(null, Validators.required),
+      'address2': new FormControl(null),
+      'email': new FormControl(null, Validators.email),
+      'password': new FormControl(null, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/))
+    })
+  }
 
   ngOnInit(): void {
+
   }
 
   signUp(){
-
+    console.log(this.signUpForm.value);
+    this.signUpForm.reset();
   }
 
 }
