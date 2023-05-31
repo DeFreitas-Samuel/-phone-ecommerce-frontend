09a5cdffbe939c309c92868c4c7024a8c37b46e5
DeFreitas-Samuel <defreitas.samuel.99@gmail.com>
2023-05-31 16:58:02 -0400

    refactor to the carousel code

--- src/app/Components/carousel/carousel.component.ts
+++ src/app/Components/carousel/carousel.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { Subject, Subscription, interval, of, switchMap, takeUntil, tap } from 'rxjs';
 import { CarouselItem } from 'src/app/interfaces/carouselItem.interface';
 
 @Component({
@@ -12,61 +12,91 @@ export class CarouselComponent implements OnInit, OnDestroy {
   @Input() carouselArray!: CarouselItem[];
   @Input() interval: number = 6000;
   currentItem!: CarouselItem;
-  currentIndex: number = 0; 
-  carouselNextItemSubscription!: Subscription | null;
+  currentIndex: number = 0;
+
+  private readonly destroyer$ = new Subject<void>();
 
   constructor() { }
 
-
-
-
-
   ngOnInit(): void {
+    this.bootstrap();
+  }
+
+  private bootstrap() {
     this.currentItem = this.carouselArray[this.currentIndex];
     this.intervalStart(this.interval);
-
   }
+
   ngOnDestroy(): void {
-      this.intervalStop()
+    this.destroyer$.next();
+    this.destroyer$.complete();
   }
 
-  intervalStart(intervalTime: number){
-    this.intervalStop()
-    this.carouselNextItemSubscription = interval(intervalTime).subscribe(()=>this.nextImage())
+  intervalStart(intervalTime: number) {
+
+    interval(intervalTime)
+      .pipe(
+        takeUntil( this.destroyer$ ),
+        tap(() => this.nextImage())
+      )
+      .subscribe()
+
+     const c$ =  of([1])
+      .pipe(
+
+      )
+      .subscribe(console.log)
+
+      const a = [1,2,3,4];
+
+      const n = a.filter((n) => n > 2)
+        .map(n => n * 2)
+        .reduce((acc,val) => acc+ val, 0)
   }
 
-  intervalStop(){
-    if(this.carouselNextItemSubscription){
-      this.carouselNextItemSubscription.unsubscribe()
-      this.carouselNextItemSubscription = null;
-    }
-  }
+  // pipe(...args:Array<Function>) {
+  //   return args.reduce((acc, val) => {
+  //     return val(acc)
+  //   }, 0 )
+  // }
 
-  nextImage(){
-    const carouselArrayLengthZeroIndexed = this.carouselArray.length -1;
-    if(this.currentIndex >= carouselArrayLengthZeroIndexed){
+  // intervalStop(){
+  //   if(this.carouselNextItemSubscription){
+  //     this.carouselNextItemSubscription.unsubscribe()
+  //     this.carouselNextItemSubscription = null;
+  //   }
+  // }
+
+  nextImage() {
+    const carouselArrayLengthZeroIndexed = this.carouselArray.length - 1;
+    if (this.currentIndex >= carouselArrayLengthZeroIndexed) {
       this.currentIndex = 0;
       this.currentItem = this.carouselArray[this.currentIndex];
-      this.intervalStart(this.interval);
+      this.restartCarousel();
     }
-    else{
+    else {
       this.currentIndex++;
       this.currentItem = this.carouselArray[this.currentIndex];
-      this.intervalStart(this.interval);
+      this.restartCarousel();
     }
   }
 
-  previousImage(){
+  previousImage() {
     const carouselArrayLength = this.carouselArray.length;
-    if(this.currentIndex <= 0){
-      this.currentIndex = carouselArrayLength-1;
+    if (this.currentIndex <= 0) {
+      this.currentIndex = carouselArrayLength - 1;
       this.currentItem = this.carouselArray[this.currentIndex];
-      this.intervalStart(this.interval);
+      this.restartCarousel();
     }
-    else{
+    else {
       this.currentIndex--;
       this.currentItem = this.carouselArray[this.currentIndex];
-      this.intervalStart(this.interval);
+      this.restartCarousel();
     }
+  }
+
+  private restartCarousel() {
+    this.destroyer$.next();
+    this.intervalStart(this.interval);
   }
 }
