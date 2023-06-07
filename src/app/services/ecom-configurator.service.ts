d1f67cb2ad1913f32391974b71db2a919974f8fa
DeFreitas-Samuel <defreitas.samuel.99@gmail.com>
2023-06-07 16:08:07 -0400

    slight style modifications

--- src/app/services/ecom-configurator.service.ts
+++ src/app/services/ecom-configurator.service.ts
@@ -23,18 +23,18 @@ export class EcomConfigurator {
         return of([
             {
                 imageUrl: 'http://localhost:8000/api/images/1.jpg',
-                alt: 'Mac',
-                linkToRedirect: 'test'
+                alt: 'MacBook Pro image',
+                linkToRedirect: 'http://localhost:4200/product/1'
             },
             {
                 imageUrl: 'http://localhost:8000/api/images/2.jpg',
                 alt: 'Mac2',
-                linkToRedirect: 'test2'
+                linkToRedirect: 'http://localhost:4200/product/2'
             },
             {
                 imageUrl: 'http://localhost:8000/api/images/3.jpg',
                 alt: 'Mac3',
-                linkToRedirect: 'test3'
+                linkToRedirect: 'http://localhost:4200/product/3'
             }
         ])
 
