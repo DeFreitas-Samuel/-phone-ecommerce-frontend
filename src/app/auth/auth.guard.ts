8a92e06850f5a35d74f6b865482b31aa63890a99
DeFreitas-Samuel <defreitas.samuel.99@gmail.com>
2023-06-19 13:25:11 -0400

    added more styles to the spinner

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -13,13 +13,13 @@ export class CanActivateIfLoggedIn implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
 
     const isLoggedIn: Boolean = !!this.authService.loggedInUserSnapshot;
-    console.log(state.url);
 
-    if ((state.url === '/login' || state.url === '/signup') && isLoggedIn) {
 
-      this.router.navigate(['/']);
-      return false;
-
+    if (state.url === '/login' || state.url === '/signup') {
+      if(isLoggedIn){
+        this.router.navigate(['/']);
+        return false;
+      }
     }
     else if (!isLoggedIn) {
       this.router.navigate(['/login']);
